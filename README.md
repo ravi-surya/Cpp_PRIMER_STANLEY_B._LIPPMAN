4fc9545042f1a6a37691294871f4f81adbcbbc66
ravi-surya <ravisuryak8@gmail.com>
2024-05-23 23:00:08 +0530

    c++

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 Hello this is my side project.
 Hopefully this will help others.
 
-I have labelled each excersize question with Ex 
+I have labelled each excersize question with "Ex" 
 Hence if you wanna search any question then hit ctrl+f and search in Ex following the question number.
+
+Also the excersizes "Ex" which has an "*" next to it ,is kinda important,
+Pay attention to it and learn it properly...
--- junky.cpp
+++ junky.cpp
@@ -2,9 +2,29 @@
 #include "Sales_item.h"
 int main()
 {
-Sales_item item1, item2;
-std::cin >> item1 >> item2; // read a pair of transactions
-std::cout << item1 + item2 << std::endl; // print their sum
+    Sales_item item,item1;
+ 
+    if(std::cin>>item)//input the first Sales_item object
+    {
+        int count =1;// have counter initialized to 1
+        while(std::cin>>item1)//input the second Sales_item object
+        {
+            if(item.isbn()== item1.isbn())
+            //check if the isbn for both are matching,if yes increament the count and add the two objects
+            {
+                count++;
+                item+=item1;
+
+            }
+            else{
+            //else     
+                std::cout<<item.isbn()<<" is the isbn and the number of transaction for this isbn is "<< count<<std::endl;
+                std::cout<<item<<std::endl;
+                item = item1;
+                count=1;
+            }
+        }
+    }   
 return 0;
 
 }
